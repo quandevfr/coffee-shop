528733546bb951abb78e1f646dd1d3a8404571be
Dev-Quan <quandev.fr@gmail.com>
2022-05-06 03:01:46 +0700

    update home

--- src/layouts/default/index.js
+++ src/layouts/default/index.js
@@ -6,6 +6,8 @@ import OutstandingCard from 'src/components/outStandingCard';
 import ProductCard from 'src/components/productCard';
 import { dataProduct } from 'src/mockData/dataMock';
 
+import bannerImg from '../../assets/images/img_banner.png';
+
 import './style.scss';
 
 const DefaultLayout = () => {
@@ -14,13 +16,17 @@ const DefaultLayout = () => {
       <div className='layoutHeader'>
         <Header />
       </div>
-      <div style={{ height: '56px' }} />
-      <div className='layoutBanner' />
+      <div className='layoutBanner'>
+        <img src={bannerImg} alt='banner' />
+      </div>
 
       <section id='products' className='layoutProduct'>
         <div className='bodyWrap'>
           <h1 className='bodyTitle'>Cà phê sữa đá</h1>
-          <p className='bodySub_title'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          <p className='bodySub_title'>
+            Được chiết xuất từ 100% hoa Hibiscus tự nhiên, không chứa caffeine cùng toppings trái cây đa dạng, Hi-Tea
+            Healthy là lựa chọn cho những ai muốn chăm sức khoẻ và yêu chiều bản thân.
+          </p>
           <div className='listCard'>
             {dataProduct.map((product, idx) => (
               <ProductCard {...product} key={idx} />
@@ -34,8 +40,7 @@ const DefaultLayout = () => {
           <p className='bodySub_title outstandingDesc'>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
             industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining.
+            scrambled it to make a type specimen book.
           </p>
           <div className='outstandingList'>
             <OutstandingCard />
